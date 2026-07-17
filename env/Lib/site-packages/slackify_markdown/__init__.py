425b8a7d5edf41b72162f2dc48e91a3b8080d424
raidenia3-oss <raidenia3@gmail.com>
2026-07-17 16:09:49 -0500

    Fix: actualizar .gitignore para excluir entornos virtuales y secretos

--- env/Lib/site-packages/slackify_markdown/__init__.py
+++
@@ -1,6 +0,0 @@
-from .service import slackify_markdown
-from typing import List
-
-__all__: List[str] = [
-    "slackify_markdown",
-]
--- env/Lib/site-packages/slackify_markdown/service.py
+++
@@ -1,8 +0,0 @@
-from slackify_markdown.slackify import SlackifyMarkdown
-
-
-def slackify_markdown(markdown: str) -> str:
-    """
-    Convert markdown to Slack-compatible markdown.
-    """
-    return SlackifyMarkdown(markdown).slackify()
